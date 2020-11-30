a508d5c913a6aaf90be09fa9f7f591e255e58ea8
Olivia Hsu <owhsu@stanford.edu>
2020-11-30 00:31:07 -0500

    Fix matcher in lowerer_impl creating tensors

--- src/lower/lowerer_impl.cpp
+++ src/lower/lowerer_impl.cpp
@@ -199,7 +199,11 @@ LowererImpl::lower(IndexStmt stmt, string name,
                                             TensorProperty::Dimension, loc);
             }
           }
-        } else {
+        }
+      /* else {
+          cout << "temporary set" << endl;
+          for (auto it = temporariesSet.begin(); it != temporariesSet.end(); ++it)
+            cout << it->getName() << endl;
           if(!util::contains(temporariesSet, n->lhs.getTensorVar())) {
             if (provGraph.hasBoundedDescendant(indexVar)) {
               auto ivars = n->lhs.getIndexVars();
@@ -214,7 +218,7 @@ LowererImpl::lower(IndexStmt stmt, string name,
                                                                          provGraph.getVarBound(indexVar)));
             }
           }
-        }
+        }*/
       }),
       function<void(const AccessNode*)>([&](const AccessNode* n) {
         auto indexVars = n->indexVars;
